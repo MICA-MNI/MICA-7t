7cc8cfe02361abc144b672290c74d20948bedaf5
rcruces <raulrcruces@gmail.com>
2024-01-18 12:09:26 -0500

    Remove dependencies on micapipe

---
+++ functions/utilities.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Utilities
+#
+
+# Print command
+function Do_cmd() {
+# do_cmd sends command to stdout before executing it.
+str="$(whoami) @ $(uname -n) $(date)"
+local l_command=""
+local l_sep=" "
+local l_index=1
+while [ ${l_index} -le $# ]; do
+    eval arg=\${$l_index}
+    if [ "$arg" = "-fake" ]; then
+      arg=""
+    fi
+    if [ "$arg" = "-no_stderr" ]; then
+      arg=""
+    fi
+    if [ "$arg" == "-log" ]; then
+      nextarg=$(("${l_index}" + 1))
+      eval logfile=\${"${nextarg}"}
+      arg=""
+      l_index=$[${l_index}+1]
+    fi
+    l_command="${l_command}${l_sep}${arg}"
+    l_sep=" "
+    l_index=$[${l_index}+1]
+   done
+if [[ ${quiet} != TRUE ]]; then echo -e "\033[38;5;118m\n${str}:\nCOMMAND -->  \033[38;5;122m${l_command}  \033[0m"; fi
+if [ -z "$TEST" ]; then $l_command; fi
+}
+
+# Print error message
+Error() {
+echo -e "\033[38;5;9m\n-------------------------------------------------------------\n\n[ ERROR ]..... $1\n
+-------------------------------------------------------------\033[0m\n"
+}
+
+# Print info message
+Info() {
+Col="38;5;75m" # Color code
+if [[ ${quiet} != TRUE ]]; then echo  -e "\033[$Col\n[ INFO ]..... $1 \033[0m"; fi
+}
